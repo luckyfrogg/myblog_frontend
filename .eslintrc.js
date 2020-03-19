014d7948c57e20984372fe67db8428077e6c6f74
HuChengzhiIsConnecting <297799265@qq.com>
2020-03-19 23:41:49 +0800

    初次提交

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+
+module.export  = {
+    rout: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        'plugin:vue/recommended'
+    ],
+    plugins: [
+        'vue'
+    ],
+    // 添加自定义规则
+    rules: {}
+}
